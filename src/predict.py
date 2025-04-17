2342c621e08549c4521500d974d66f46289823c7
andreasdarsa <andreasd@csd.auth.gr>
2025-04-17 17:59:16 +0300

    Multiple ticker options added

--- src/predict.py
+++ src/predict.py
@@ -2,10 +2,10 @@ import data_loader as dl
 import model as m
 from sklearn.model_selection import train_test_split
 
-def predict():
+def predict(ticker: str):
     data = dl.fetch_data()
     X = data[["Open", "High", "Low", "Volume"]]
-    y = data[("Close","AAPL")]
+    y = data[("Close",ticker)]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=45, shuffle=False)
     model = m.train_model(X_train, y_train)
     print("Model evaluation results: ", m.evaluate_model(model, X_test, y_test))
@@ -13,4 +13,5 @@ def predict():
     print(predicted_vals[0])
 
 if __name__ == "__main__":
-    predict()
+    t = input("Enter any valid ticker (AAPL/MSFT/GOOGL/TSLA): ")
+    predict(ticker=t)
